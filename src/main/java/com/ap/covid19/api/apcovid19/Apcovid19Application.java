149885f0c7a2f143f105305ccf7a4cefcadbca16
RicardoGaynor <ricardogaynorgaynoruwi@gmail.com>
2020-10-22 20:51:09 -0500

    Initial commit

---
+++ src/main/java/com/ap/covid19/api/apcovid19/Apcovid19Application.java
@@ -0,0 +1,13 @@
+package com.ap.covid19.api.apcovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apcovid19Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apcovid19Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ap/covid19/api/apcovid19/Apcovid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.covid19.api.apcovid19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apcovid19ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
